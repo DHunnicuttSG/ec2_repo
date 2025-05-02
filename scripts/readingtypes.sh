e53cf45b14c6a4f0230ccfb09186434e29b29c01
dhunnicuttsg <dhunnicutt527@gmail.com>
2025-05-02 14:25:33 -0500

    adding the datatype example

---
+++ scripts/readingtypes.sh
@@ -0,0 +1,12 @@
+echo Hello, enter in an integer:
+read myInt
+echo Enter in a float:
+read myFloat
+echo Enter in a string:
+read myString
+
+echo $myInt + $myFloat
+
+sum=$(echo "$myInt + $myFloat" | bc)
+
+echo $myString
