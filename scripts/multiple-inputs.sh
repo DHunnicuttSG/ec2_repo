cb908d106a9ec08ec52144e4d12867871b996348
dhunnicuttsg <dhunnicutt527@gmail.com>
2025-05-02 13:38:01 -0500

    updated script for multi-parameters

--- scripts/multiple-inputs.sh
+++ scripts/multiple-inputs.sh
@@ -1,7 +1,23 @@
-for i in "$@"; do 
-if [[ "$i" =~ ^[0-9]+$ ]]; then 
-echo "Contains a number" 
-else 
-echo "Doesn't contain a number" 
-fi 
+#!/bin/bash
+
+# This script demonstrates how to accept any number of parameters from the command line.
+
+# Accessing all parameters
+echo "All parameters: $@"
+
+# Accessing the number of parameters
+echo "Number of parameters: $#"
+
+# Accessing individual parameters using a loop
+echo "Individual parameters:"
+for i in "$@"
+do
+  echo "  $i"
+done
+
+# Accessing individual parameters by index
+echo "Parameters by index:"
+for ((i=1; i<=$#; i++))
+do
+  echo "  Parameter $i: ${!i}"  # Or  echo "  Parameter $i: $(eval echo \$$i)"
 done
